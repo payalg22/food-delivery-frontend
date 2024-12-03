432049e1036014cb1122ec3eff7f7cdf7696a62f
Payal Gaikwad <gaikwadpayalr@gmail.com>
2024-12-03 23:06:32 +0530

    user folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Login from "./UserAuthentication/Login.jsx";
+import Login from "./user/Login.jsx";
 import Home from "./home/Home.jsx";
 import Profile from "./profile/EditProfile.jsx";
 import Product from "./product/Product.jsx";
 import Address from "./address/Address.jsx";
 import NotFound from "./notfound/NotFound.jsx";
-import Register from "./userAuthentication/Register.jsx";
+import Register from "./user/Register.jsx";
 
 export { Login, Register, Home, Profile, Product, Address, NotFound };
